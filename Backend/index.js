21e691b7415a9e1ffb3b825d963bf6ecec9f4436
opax <opaaaax@gmail.com>
2025-11-29 22:09:01 +0100

    Add backend

---
+++ Backend/index.js
@@ -0,0 +1,84 @@
+const vision = require('@google-cloud/vision');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const KEY_FILE_PATH = './bhla-479720-4d29cb0d1b9f.json';
+
+app.use(cors());
+app.use(express.json());
+
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: KEY_FILE_PATH
+});
+
+async function isFromShein(imagePath) {
+    try {
+        const [result] = await client.webDetection(imagePath);
+        const webDetection = result.webDetection;
+    
+        if (!webDetection) {
+          return { isShein: false, url: null };
+        }
+    
+        const targetKeywords = ['shein','aliexpress','romwe'];
+    
+        const allItems = [
+          ...(webDetection.pagesWithMatchingImages || []),
+          ...(webDetection.partialMatchingImages || []),
+          ...(webDetection.visuallySimilarImages || [])
+        ];
+    
+        const foundItem = allItems.find(item => {
+          const url = item.url ? item.url.toLowerCase() : '';
+          return targetKeywords.some(keyword => url.includes(keyword));
+        });
+    
+        if (foundItem) {
+          return { 
+            isShein: true, 
+            url: foundItem.url 
+          };
+        }
+    
+        return { isShein: false, url: null };
+    
+      } catch (error) {
+        console.error("Błąd:", error);
+        return { isShein: false, url: null, error: error.message };
+      }
+}
+
+app.post('/api/analyze', async (req, res) => {
+    const { imageUrl } = req.body;
+
+    if(!imageUrl) {
+        return res.status(400).json({
+            error: 'Missing field: imageUrl is required'
+        });
+    }
+
+    try{
+        const result = await isFromShein(imageUrl);
+        res.json(result);
+    } catch (err){
+        res.status(500).json({
+            error: 'Internal server error',
+            details: error.message
+        })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+})
+
+//test
+/*
+(async () =>{
+    //const res = await isFromShein("https://img.ltwebstatic.com/v4/j/pi/2025/10/17/83/17606916318bffa0b3bb502ed6272bed8987a928a4_thumbnail_900x.webp"); 
+    const res = await isFromShein("https://images1.vinted.net/t/06_02068_VodyJbvhSxjfFNiiKphNaAVQ/f800/1764447724.webp?s=762d1931660d5a467362879e6fcd339f3ff0d326");
+    console.log(res);
+})();
+*/
